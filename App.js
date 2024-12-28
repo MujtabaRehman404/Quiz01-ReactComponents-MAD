7591c4bf6fd333dfc29626be27f60f320c3b9fc6
Mujtaba Rehman <159465298+MujtabaRehman404@users.noreply.github.com>
2024-12-28 14:38:29 +0500

    first commit
    commit consiting of all the respective files with code.

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import ItemList from './ItemList';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <ItemList />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f8f9fa',
+  },
+});
+
+export default App;
+
